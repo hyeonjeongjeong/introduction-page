033b5571f851e9535edd8a88dbab253c2bb947e6
mandyee <minju4144@gmail.com>
2022-04-06 06:09:30 +0900

    Add add item function

--- js/kanbanboard.js
+++ js/kanbanboard.js
@@ -1,4 +1,6 @@
-﻿let item = null;
+﻿// drag & drop
+
+let item = null;
 const allItem = document.querySelectorAll(".kanban__item");
 const allColumn = document.querySelectorAll(".kanban__column");
 
@@ -41,4 +43,35 @@ function dragDrop() {
 	this.style.border = "none";
 	this.children[1].appendChild(item);
 	console.log("dropped");
+}
+
+// Add Item
+
+const allAddBtn = document.querySelectorAll(".kanban__add-item");
+
+allAddBtn.forEach((addBtn) => {
+	addBtn.addEventListener("click", addItem);
+})
+
+function addItem() {
+	const itemDiv = document.createElement("div");
+	itemDiv.className = "kanban__item";
+	itemDiv.setAttribute("draggable", "true");
+	itemDiv.addEventListener("dragstart", dragStart);
+	itemDiv.addEventListener("dragend", dragEnd);
+
+	const itemInputDiv = document.createElement("div");
+	itemInputDiv.className = "kanban__item-input";
+	itemInputDiv.setAttribute("contenteditable", true);
+	itemDiv.appendChild(itemInputDiv);
+
+	const dropzoneDiv = document.createElement("div");
+	dropzoneDiv.className = "kanban__dropzone";
+	itemDiv.appendChild(dropzoneDiv);
+
+	let column = document.querySelector(".kanban__items");
+	column = this.previousElementSibling;
+	column.appendChild(itemDiv);
+
+	console.log("add");
 }
