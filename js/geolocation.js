84a3e70b67307d552842fad21685f4cd82834bf0
ababqw123 <96455920+ababqw123@users.noreply.github.com>
2022-04-08 16:25:21 +0900

    로드맵

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,10 +1,10 @@
 ﻿window.onload = function ()
 {
 
-        var mapContainer = document.getElementById('map'),
-        mapOption = {
-            center: new kakao.maps.LatLng(37.583749, 126.999955),
-            level: 5
+    var mapContainer = document.getElementById('map'),
+    mapOption = {
+        center: new kakao.maps.LatLng(37.583749, 126.999955),
+        level: 5
         };
     var map = new kakao.maps.Map(mapContainer, mapOption);
     var markerPosition = new kakao.maps.LatLng(37.583749, 126.999955);
@@ -19,8 +19,8 @@
     let roadviewbox = document.getElementById("roadview_box");
     let change = 0;
 
+
     function roadview() {
-        if (change == 0) {
             var roadview = new kakao.maps.Roadview(roadviewContainer); //로드뷰 객체
             var roadviewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
             var position = point;
@@ -28,20 +28,10 @@
             roadviewClient.getNearestPanoId(position, 50, function (panoId) {
                 roadview.setPanoId(panoId, position); //panoId와 중심좌표를 통해 로드뷰 실행
             });
-            roadviewbox.style.display = "block";
-            mapContainer.style.display = "none";
-            change = 1;
-        }
-        else {
-            roadviewbox.style.display = "none";
-            mapContainer.style.display = "block";
-            change = 0;
-        }
-
         
     }
 
-
+    roadview();
 
     function click_move(event) {
         marker.setMap(null);
@@ -49,55 +39,30 @@
         let loc = event.target.getAttribute("value");
         var geocoder = new kakao.maps.services.Geocoder();
 
-        // 주소로 좌표를 검색합니다
         geocoder.addressSearch(loc, function (result, status) {
 
-            // 정상적으로 검색이 완료됐으면
             if (status === kakao.maps.services.Status.OK) {
 
                 var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
                 point = coords;
-                // 결과값으로 받은 위치를 마커로 표시합니다
                 marker = new kakao.maps.Marker({
                     map: map,
                     position: coords
                 });
-                // 인포윈도우로 장소에 대한 설명을 표시합니다
                 infowindow = new kakao.maps.InfoWindow({
                     content: '<div style="width:150px;text-align:center;padding:6px 0;">' + event.target.textContent+'</div>'
                 });
                 infowindow.open(map, marker);
-
-                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                roadview();
                 map.setCenter(coords);
             }
         });
     }
 
 
-    function location() {
-        if (navigator.geolocation) {
-
-            navigator.geolocation.getCurrentPosition(function (position) {
-
-                var lat = position.coords.latitude, // 위도
-                    lon = position.coords.longitude; // 경도
-
-                var locPosition = new kakao.maps.LatLng(lat, lon),
-                    message = "현재위치입니다";
-                point = locPosition;
-                displayMarker(locPosition, message);
-            });
-
-        } else {
-            var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
-                message = 'geolocation을 사용할수 없어요..'
-            displayMarker(locPosition, message);
-        }
-    }
-
     //마커 및 위치 이동
     function displayMarker(locPosition, message) {
+        roadview();
         marker.setMap(null);
         marker = new kakao.maps.Marker({
             map: map,
@@ -118,12 +83,44 @@
 
 
     let now = document.querySelector(".now");
-    now.addEventListener("click", location);
+    now.addEventListener("click", () => {
+        if (navigator.geolocation) {
+
+            navigator.geolocation.getCurrentPosition(function (position) {
+
+                var lat = position.coords.latitude, // 위도
+                    lon = position.coords.longitude; // 경도
+
+                var locPosition = new kakao.maps.LatLng(lat, lon),
+                    message = "현재위치입니다";
+                point = locPosition;
+                displayMarker(locPosition, message);
+            });
+
+        } else {
+            var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+                message = 'geolocation을 사용할수 없어요..'
+            displayMarker(locPosition, message);
+        }
+    });
 
     let click_btn = document.querySelector("#move");
     click_btn.addEventListener("click", click_move);
 
     let view = document.querySelector(".view");
-    view.addEventListener("click", roadview);
+    view.addEventListener("click", () => {
+        if (change == 0) {
+            roadviewbox.style.display = "block";
+            mapContainer.style.display = "none";
+            document.querySelector('.view').textContent="지도"
+            change = 1;
+        }
+        else {
+            roadviewbox.style.display = "none";
+            mapContainer.style.display = "block";
+            document.querySelector('.view').textContent = "로드뷰"
+            change = 0;
+        }
+    });
 }
 
